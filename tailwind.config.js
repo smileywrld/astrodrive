4b77c60980e29ef60ce2fada61edac3b22427de0
smileywrld <ismaileyyunusa@gmail.com>
2025-07-01 18:04:23 +0100

    Enhance index.html and tailwind.config.js: update mobile menu styles and add animation for improved user experience

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 module.exports = {
 	content: ["./index.html"],
 	theme: {
-		extend: {},
-		screens: {
-			widescreen: { raw: "(min-aspect-ratio: 3/2)" },
-			tallscreen: { raw: "(min-aspect-ratio: 13/20)" },
+		extend: {
+			screens: {
+				widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+				tallscreen: { raw: "(min-aspect-ratio: 13/20)" },
+			},
+			keyframes: {
+				"open-menu": {
+					"0%": { transform: "scaleY(0)" },
+					"80%": { transform: "scaleY(1.2)" },
+					"100%": { transform: "scaleY(1)" },
+				},
+			},
+
+			animation: {
+				"open-menu": "open-menu 0.5s ease-in-out forwards",
+			},
 		},
 	},
 	plugins: [],
